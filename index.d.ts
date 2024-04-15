e2ae89d77819ddbf7139e4d918bba770a1c54098
Teun Wolbert@ubuntu <teunwolbert@outlook.com>
2024-04-16 00:40:10 +0200

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
+export {}
+
 // Types for class T1
-interface T1Props {
+export interface T1Props {
     remoteUrl: string,
     T1auth: T1Auth
     bucketAuth?: BucketAuth
@@ -44,23 +46,11 @@ interface Bucket {
     updatedAt: string,
 }
 
-declare class T1 {
+export declare class T1 {
     constructor(props: T1Props)
     createBucket: (props: createBucketProps) => Promise<Bucket>
     getFile: (fileId: number) => Promise<ArrayBuffer>
     uploadFile: (props: UploadFileProps) => Promise<boolean>
     createBucketInstance: (props: CreateBucketInstanceProps) => Promise<boolean>
     deleteBucket: (props: deleteBucketProps) => Promise<boolean>
-}
-
-declare namespace WLBT {
-    export { T1 }
-    export { T1Props }
-    export { T1Auth }
-    export { BucketAuth }
-    export { UploadFileProps }
-    export { CreateBucketInstanceProps }
-    export { createBucketProps }
-    export { deleteBucketProps }
-    export { Bucket }
 }
